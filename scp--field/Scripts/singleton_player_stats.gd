7a36ba0a98d643497247055e2f56d4072ee2c42a
AAP-01 <jbthememer43@gmail.com>
2026-08-05 01:04:05 -0400

    Made some rooms and adjusted scaling of the camera and entity sizes. Changed player speed

--- scp--field/Scripts/singleton_player_stats.gd
+++ scp--field/Scripts/singleton_player_stats.gd
@@ -3,5 +3,5 @@ class_name PlayerStats
 
 var health : float = 100.0
 var ammunition : int = 120
-var walk_speed : float = 150.0
-var sprint_speed : float = 250.0
+var walk_speed : float = 50.0
+var sprint_speed : float = 100.0
