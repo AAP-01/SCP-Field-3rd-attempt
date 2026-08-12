43899b08901b9a18e2315ef30cc2388c6cedc496
AAP-01 <jbthememer43@gmail.com>
2026-08-12 12:15:41 -0400

    The interaction prompt in field codes and the extraction point now appear and disappear if the player is in or out of their hitbox

--- scp--field/Scripts/extraction_point.gd
+++ scp--field/Scripts/extraction_point.gd
@@ -1,10 +1,22 @@
 extends Area2D
 
+@onready var interact_prompt: Node2D = $"Interact Prompt"
+
 func _on_body_entered(body: Node2D) -> void:	# This triggers when something hits this
 	# Add a future mechanic here that shows the player a prompt to extract
 	if body.name == "Player":
 		print("Reached extraction point")
-		bring_to_game_over()
+		show_prompt()
+		
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		hide_prompt()
+	
+func show_prompt() -> void:
+	interact_prompt.visible = true
+	
+func hide_prompt() -> void:
+	interact_prompt.visible = false
 		
 func bring_to_game_over() -> void:
 	get_tree().call_deferred("change_scene_to_file", "res://Scenes/game_over.tscn")
--- scp--field/Scripts/scp_field_code.gd
+++ scp--field/Scripts/scp_field_code.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var field_code : FieldCodeData
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var interact_prompt: Node2D = $"Interact Prompt"
 
 func setup(field_code : FieldCodeData) -> void:
 	self.field_code = field_code
@@ -12,8 +13,15 @@ func _on_body_entered(body: Node2D) -> void:
 		print("Found a field code")
 		show_prompt()
 		
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		hide_prompt()
+	
 func show_prompt() -> void:
-	pass
+	interact_prompt.visible = true
+	
+func hide_prompt() -> void:
+	interact_prompt.visible = false
 	
 func interact() -> void:
 	pass
