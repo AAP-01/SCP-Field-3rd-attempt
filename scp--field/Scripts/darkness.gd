a6429a8e899e8033aac21eac26639944504788cc
AAP-01 <jbthememer43@gmail.com>
2026-09-02 01:41:00 -0400

    Darkness now only runs its hitbox code when the player touches it (spent hours trying to fix it not realizing it never checked if it was a player touching it :skull:)

--- scp--field/Scripts/darkness.gd
+++ scp--field/Scripts/darkness.gd
@@ -2,12 +2,21 @@ extends Area_threat
 
 class_name Darkness
 
+@onready var countdown_before_attacking: Timer = $"Countdown before attacking"
+
 func _on_body_entered(body: Node2D) -> void:
-	print("Entered an AreaThreat: " + threat_name)
-	print("Darkness is attacking")
+	if body.name == "Player":
+		print("Entered an AreaThreat: " + threat_name)
+		print("Darkness is attacking")
+		countdown_before_attacking.start()
 	
 func _on_body_exited(body: Node2D) -> void:
 	print("You left an AreaThreat: " + name)
+	countdown_before_attacking.stop()
 	
 func attack() -> void:
 	pass
+	
+func _on_countdown_before_attacking_timeout() -> void:
+	SingletonPlayerStats.health -= 10.0
+	print(SingletonPlayerStats.health)
