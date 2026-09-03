aea4d09ca95b5e694f8029157064c29be19c4886
AAP-01 <jbthememer43@gmail.com>
2026-09-03 02:16:46 -0400

    Darkness now attacks the user after staying in the area for too long

--- scp--field/Scripts/darkness.gd
+++ scp--field/Scripts/darkness.gd
@@ -3,20 +3,25 @@ extends Area_threat
 class_name Darkness
 
 @onready var countdown_before_attacking: Timer = $"Countdown before attacking"
+@onready var attack_speed_timer: Timer = $"Attack Speed"
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		print("Entered an AreaThreat: " + threat_name)
 		print("Darkness is attacking")
-		countdown_before_attacking.start()
+		if true:	# To replace: run if the area is dark
+			countdown_before_attacking.start()
 	
 func _on_body_exited(body: Node2D) -> void:
 	print("You left an AreaThreat: " + name)
 	countdown_before_attacking.stop()
+	attack_speed_timer.stop()
 	
-func attack() -> void:
-	pass
-	
+# Give the player time to leave
 func _on_countdown_before_attacking_timeout() -> void:
-	SingletonPlayerStats.health -= 10.0
+	attack_speed_timer.start()
+	
+# Start attacking if they don't leave
+func _on_attack_speed_timeout() -> void:
+	SingletonPlayerStats.health -= damage
 	print(SingletonPlayerStats.health)
