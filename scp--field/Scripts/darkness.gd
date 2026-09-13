f2ddec7e964db0ce28d7ecd34f4a8cb748024584
AAP-01 <jbthememer43@gmail.com>
2026-09-13 02:38:38 -0400

    Darkness now only attacks if there's no light and the player is in its area

--- scp--field/Scripts/darkness.gd
+++ scp--field/Scripts/darkness.gd
@@ -4,24 +4,42 @@ class_name Darkness
 
 @onready var countdown_before_attacking: Timer = $"Countdown before attacking"
 @onready var attack_speed_timer: Timer = $"Attack Speed"
+@onready var area_threat: Area_threat = $"."
+@onready var lantern = $"../../Player/Lantern"	# It goes back two times because Darkness is a child of Game Manager. Kepp that in mind
+var is_in_area : bool = false
+
+signal darkness_area_entered
+
+func _ready() -> void:
+	# Entering Darkness' area and toggling the lantern makes the game check if both conditions allow it to attack
+	darkness_area_entered.connect(attack)
+	lantern.light_activated_or_deactivated.connect(attack)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		print("Entered an AreaThreat: " + threat_name)
-		print("Darkness is attacking")
-		if true:	# To replace: run if the area is dark
-			countdown_before_attacking.start()
-	
+		is_in_area = true
+		darkness_area_entered.emit()
+		
 func _on_body_exited(body: Node2D) -> void:
-	print("You left an AreaThreat: " + name)
-	countdown_before_attacking.stop()
-	attack_speed_timer.stop()
-	
+	if body.name == "Player":
+		print("You left an AreaThreat: " + name)
+		is_in_area = false
+		darkness_area_entered.emit()
+		
+func attack():
+	if !SingletonPlayerStats.is_light_on and is_in_area:
+		countdown_before_attacking.start()
+	else:
+		countdown_before_attacking.stop()
+		attack_speed_timer.stop()
+		
 # Give the player time to leave
 func _on_countdown_before_attacking_timeout() -> void:
 	attack_speed_timer.start()
 	
 # Start attacking if they don't leave
 func _on_attack_speed_timeout() -> void:
+	print("Darkness is attacking")
 	SingletonPlayerStats.health -= damage
 	print(SingletonPlayerStats.health)
--- scp--field/Scripts/lantern.gd
+++ scp--field/Scripts/lantern.gd
@@ -4,6 +4,8 @@ var power : float = 100.0
 @onready var point_light_2d: PointLight2D = $PointLight2D
 @onready var timer: Timer = $Timer
 
+signal light_activated_or_deactivated
+
 func _ready() -> void:
 	timer.start()
 
@@ -13,14 +15,20 @@ func _process(delta: float) -> void:
 func activate() -> void:
 	if power <= 0:
 		point_light_2d.enabled = false
+		SingletonPlayerStats.is_light_on = false
 		
 	if Input.is_action_just_pressed("Light"):
 		if point_light_2d.enabled:
 			point_light_2d.enabled = false
+			SingletonPlayerStats.is_light_on = false
 			timer.paused = true
+			light_activated_or_deactivated.emit()
 		else:
 			point_light_2d.enabled = true
+			SingletonPlayerStats.is_light_on = true
 			timer.paused = false
+			light_activated_or_deactivated.emit()
+			
 		
 func _on_timer_timeout() -> void:
 	if power > 0:
